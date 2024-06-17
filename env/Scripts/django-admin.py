576f84fbaf73d07fe2a58e5133485afd1f476f4a
Furkan CICEK <furkanizm@icloud.com>
2024-06-17 13:39:14 +0300

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\frknc\OneDrive\Masaüstü\rentacar-project\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
